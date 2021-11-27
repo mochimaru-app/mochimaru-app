dd4e192a0327455ff4fc4843dc6379c7286cd5a2
Sada Yoshiki <a5819053@aoyama.jp>
2021-11-27 19:07:37 +0900

    ログイン途中経過

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,78 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link>|
+
+      <router-link to="/BeforeSignIn">Before</router-link>|
+      <router-link to="/AfterSignIn">After</router-link>
     </div>
     <router-view />
+    <div v-if="isLoggin"> している
+      <button @click="logOut">ログアウト</button>
+    </div>
+    <div v-else> してない
+      <button @click="login">ログイン</button>
+      
+      <!-- <div class="signed-in-user-profile" v-if="user">
+      <div>ようこそ{{user.displayName}}</div>
+      </div>
+      <div v-else>No User Signed In</div> -->
+    </div>
+
+
   </div>
 </template>
+
+<script>
+import firebase from "firebase"
+// import router from "vue-router";
+
+export default {
+  data() {
+    return {
+      isLoggin: false,
+    }
+  },
+  // created(){
+  //       firebase.auth().onAuthStateChanged((user)=>{
+  //           if(user){
+  //               this.user=user
+  //           }else{
+  //               this.user=null
+  //           }
+  //       })
+  // },
+  // computed: {
+  //   user() {
+  //     return this.$auth.cuurentUser
+  //   },
+  // },
+  methods: {
+    login() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          console.log({ result })
+          if (result.user) {
+            this.isLoggin = true
+            // $router.push("/About");
+            this.$router.push("/about")
+          }
+        })
+    },
+    logOut() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.isLoggin = false
+        })
+    },
+  },
+}
+</script>
 
 <style>
 #app {
