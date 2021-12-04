f3de4c421cf4c42248e72e2a4c23c30b7b9c3963
NozomiTachizawa <mugikko27@gmail.com>
2021-12-05 00:35:29 +0900

    投稿ページのmap途中

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -50,12 +50,20 @@
     </div>
     <div>
       <h4>map</h4>
+      <input type="text" v-model="mapAddress" />
+      <button type="button" @click="mapSearch">検索</button>
+      <div>
+        緯度：<input type="text" v-model="postData.lat" ref="lat" />
+        経度：<input type="text" v-model="postData.lng" ref="lng" />
+      </div>
+      <div id="map" style="height: 400px; width: 500px"></div>
     </div>
     <button v-on:click="post">投稿！</button>
   </div>
 </template>
 
 <script>
+/* global google */
 export default {
   data() {
     return {
@@ -65,12 +73,67 @@ export default {
         money: "",
         recommend: "",
         checkValue: "",
+        lat: 0,
+        lng: 0,
       },
+      map: null,
+      marker: null,
+      geocoder: null,
+      mapAddress: "",
+      aft: false,
     }
+  },
+  mounted() {
+    this.map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 35.6803997, lng: 139.7690174 },
+      zoom: 12,
+    })
+    this.geocoder = new google.maps.Geocoder()
   },
   methods: {
     setCheckValue: function (value) {
       this.postData.checkValue = value
+    },
+    mapSearch() {
+      this.geocoder.geocode(
+        {
+          address: this.mapAddress,
+        },
+        (results, status) => {
+          if (status === google.maps.GeocoderStatus.OK) {
+            if (this.aft === true) {
+              this.marker.setMap(null)
+            }
+            this.map.setCenter(results[0].geometry.location)
+            this.marker = new google.maps.Marker({
+              map: this.map,
+              position: results[0].geometry.location,
+              draggable: true,
+            })
+            this.postData.lat = results[0].geometry.location.lat()
+            this.postData.lng = results[0].geometry.location.lng()
+            this.aft = true
+            // マーカーのドロップ（ドラッグ終了）時のイベント
+            google.maps.event.addListener(
+              this.marker,
+              "dragend",
+              function (ev) {
+                console.log(ev.latLng.lat())
+                console.log(ev.latLng.lng())
+                const lat = ev.latLng.lat()
+                const lng = ev.latLng.lng()
+                // イベントの引数evの、プロパティ.latLngが緯度経度
+                console.log(lat)
+                console.log(lng)
+                this.$ref.lat.value = lat
+                this.$ref.lng.value = lng
+                // this.postData.lat = lat
+                // this.postData.lng = lng
+              }
+            )
+          }
+        }
+      )
     },
     post: function () {
       console.log(this.postData)
@@ -98,5 +161,8 @@ export default {
 .stars label:hover ~ label,
 .stars input[type="radio"]:checked ~ label {
   color: #f8c601;
+}
+#map {
+  margin: 0px auto;
 }
 </style>
